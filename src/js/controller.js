2cac1aa3232bd4fbc4ddd5905f034db087243a3f
gbloch <gaetan.bloch@gmail.com>
2021-01-31 06:48:34 +0100

    Get a recipe by its id from the list of recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 const recipeContainer = document.querySelector('.recipe');
 
+const URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
 const timeout = (seconds) => new Promise((_, reject) => {
   setTimeout(() => {
     reject(new Error(`Request took too long! Timeout after ${seconds} seconds`));
@@ -8,12 +10,15 @@ const timeout = (seconds) => new Promise((_, reject) => {
 
 // https://forkify-api.herokuapp.com/v2
 
-const getRecipes = async (search) => {
-  const res = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}`,
-  );
+const getJSON = async (url) => {
+  const res = await fetch(url);
   const data = await res.json();
   if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  return data;
+};
+
+const getRecipes = async (search) => {
+  const data = await getJSON(`${URL}?search=${search}`);
   return Array.from(data.data.recipes, (recipe) => ({
     publisher: recipe.publisher,
     title: recipe.title,
@@ -22,4 +27,21 @@ const getRecipes = async (search) => {
   }));
 };
 
-getRecipes('pizza').then(console.log);
+const getRecipe = async (id) => {
+  const data = await getJSON(`${URL}/${id}`);
+  const { recipe } = data.data;
+  return {
+    publisher: recipe.publisher,
+    title: recipe.title,
+    id: recipe.id,
+    ingredients: recipe.ingredients,
+    servings: recipe.servings,
+    imageUrl: recipe.image_url,
+    sourceUrl: recipe.source_url,
+    cookingTime: recipe.cooking_time,
+  };
+};
+
+getRecipes('pizza').then((recipes) => {
+  getRecipe(recipes[0].id).then(console.log);
+});
