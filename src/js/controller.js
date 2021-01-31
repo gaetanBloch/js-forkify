a0abe3d8196b4f168e06e9b05f7dd8b20f648914
gbloch <gaetan.bloch@gmail.com>
2021-01-31 05:54:06 +0100

    Get the list of recipes from the API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,25 @@
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+const timeout = (seconds) => new Promise((_, reject) => {
+  setTimeout(() => {
+    reject(new Error(`Request took too long! Timeout after ${seconds} seconds`));
+  }, seconds * 1000);
+});
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+const getRecipes = async (search) => {
+  const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}`,
+  );
+  const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  return Array.from(data.data.recipes, (recipe) => ({
+    publisher: recipe.publisher,
+    title: recipe.title,
+    id: recipe.id,
+    imageUrl: recipe.image_url,
+  }));
+};
+
+getRecipes('pizza').then(console.log);
